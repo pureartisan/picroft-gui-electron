e28e31dc620d58eca2d9f2f010a955344e436b87
Prageeth Silva <prageethsilva@gmail.com>
2020-11-26 00:35:24 +0100

    add boiler plate react app

---
+++ src/app/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import './styles.scss';
+
+const App = () => (
+  <div>
+    <h1>Hello, Electron!</h1>
+
+    <p>Lorem ipsum</p>
+  </div>
+);
+
+export default App;
